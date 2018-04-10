8450375a85a7f8e60d85e32f5a710ab5b52aa545
Rius98 <riusputra200@gmail.com>
2018-04-11 00:55:15 +0700

    AI update
    Tambah Wave Spawner

---
+++ Assets/Master.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Master: MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public float timeBeteweenWaves = 5f;
+    private float countdown = 2f;
+    
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBeteweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    } 
+    void SpawnWave()
+    {
+        Debug.Log("Wave Incoming");
+    }
+}
---
+++ Assets/WaveSpawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WaveSpawn : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBeteweenWaves = 5f;
+    private float countdown = 2f;
+    private int WaveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBeteweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    }
+    void SpawnWave()
+    {
+        for (int i = 0; i < WaveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        WaveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position,spawnPoint.rotation);
+    }
+}
+
+
