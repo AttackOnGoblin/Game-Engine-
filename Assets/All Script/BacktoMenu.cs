6e6c20a89ec68ff7785f967f3847a71f6a58488b
Rius98 <36913111+Rius98@users.noreply.github.com>
2018-07-16 23:05:53 +0700

    v1.0
    Added Balancing and Background Music

---
+++ Assets/All Script/BacktoMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BacktoMenu : MonoBehaviour {
+    public string leveltoload = "Main Menu";
+
+    public void BackMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(leveltoload);    
+    }
+
+
+}
---
+++ Assets/All Script/Credit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Credit : MonoBehaviour {
+    public string leveltoload = "Credit Scene";
+
+        public void play ()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(leveltoload);
+    }
+}
